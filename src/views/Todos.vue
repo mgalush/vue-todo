0d5212888afbd32e2732d4751b895625a446953a
Melissa Galush <galushmn@gmail.com>
2019-12-12 23:16:21 -0500

    Added ability to complete and delete todos

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,11 +1,12 @@
 <template>
 <div>
-<h2>This is our todos </h2>
-<input type="text" v-model="todo">
+<h2>To-do List:</h2>
+<input type="text" v-model="todo.text">
 <button v-on:click="createTodo">Create Todo</button>
 <ul>
-    <li v-for="todo in todos">
-    {{ todo }}
+    <li v-for="(todo, index) in todos" v-on:click="completeTodo(todo)">
+    <span v-bind:class="{completed: todo.isCompleted}"> {{ todo.text }} </span>
+    <button v-on:click.stop="deleteTodo(index)">Delete</button>
     </li>
 </ul>
 </div>
@@ -16,20 +17,36 @@
             name: 'todos',
             data: function () {
                 return {
-                    todo: '',
+                    todo: this.newTodo(),
                     todos: []
                 };
             },
             methods: {
                 createTodo: function() {
                     this.todos.push(this.todo);
-                    this.todo = '';
-
+                    this.todo = this.newTodo();
+                },
+                deleteTodo: function(index) {
+                  this.todos.splice(index, 1);
+                },
+                completeTodo: function(todo) {
+                    todo.isCompleted = !todo.isCompleted;
+                },
+                newTodo: function() {
+                        return {
+                        text: '',
+                        isCompleted: false
+                    };
                 }
             }
         }
     </script>
 
     <style lang="css">
-
+ul {
+    list-style: none;
+}
+.completed {
+    text-decoration: line-through;
+}
     </style>
