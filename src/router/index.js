83658cf8d224d5541fcc9d5fb33582889ffe946f
Melissa Galush <galushmn@gmail.com>
2019-12-12 22:36:45 -0500

    Created todo component

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Todos from '../views/Todos.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'Todos',
+    component: Todos
   },
   {
     path: '/about',
---
+++ src/views/Todos.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+<h2>This is our todos </h2>
+<input type="text" v-model="todo">
+<button v-on:click="createTodo">Create Todo</button>
+<ul>
+    <li v-for="todo in todos">
+    {{ todo }}
+    </li>
+</ul>
+</div>
+</template>
+
+    <script>
+        export default {
+            name: 'todos',
+            data: function () {
+                return {
+                    todo: '',
+                    todos: []
+                };
+            },
+            methods: {
+                createTodo: function() {
+                    this.todos.push(this.todo);
+                    this.todo = '';
+
+                }
+            }
+        }
+    </script>
+
+    <style lang="css">
+
+    </style>
